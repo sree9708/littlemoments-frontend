749dbb5650dc6effa1b10adbdbbcd15c9260cd87
musaffarulislam <musaffarulislam@gmail.com>
2024-02-01 10:06:42 +0530

    feat: frontend codes

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    domains: ["image.tmdb.org", "images.pexels.com", "littlemoments.nyc3.digitaloceanspaces.com"],
-  },
-}
-
-module.exports = nextConfig
