58cb59db910df01ed9fa86c2da7e4745f3885a80
musaffarulislam <musaffarulislam@gmail.com>
2024-02-20 16:56:55 +0530

    feat: cards to add a empty array in place

--- src/app/add-place/upload-images/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import UploadImagesLeftSide from "@/layouts/AddPlacePage/UploadImages/UploadImagesLeftSide"
-
-const Page = () => {
-  return <UploadImagesLeftSide />
-}
-
-export default Page
--- src/components/PlacesPage/[id]/RelatedActivities/Title.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export const Title = () => {
-  return <div className="text-title-md font-title font-bold">Related Activities</div>
-}
