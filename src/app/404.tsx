2df46ba67cd0ad35a777381822aac52b69713d4f
musaffarulislam <musaffarulislam@gmail.com>
2024-02-20 12:00:54 +0530

    feat: Remove _document.tsx file

--- src/app/404.tsx
+++ src/app/404.tsx
@@ -11,7 +11,7 @@ const NotFound = () => {
                         <div className="">
                             <h1 className="my-2 text-gray-800 font-bold text-2xl">
                                 Looks like you've found the
-                                doorway to the great nothing
+                                Doorway to the great nothing
                             </h1>
                             <p className="my-2 text-gray-800">Sorry about that! Please visit our hompage to get where you need to go.</p>
                             <Link href='/' ><button className="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">Home</button></Link>
