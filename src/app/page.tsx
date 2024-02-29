d1270f5f6ff1b6319be5e7f461fcd8cea8343004
musaffarulislam <musaffarulislam@gmail.com>
2024-02-29 17:54:32 +0530

    feat: for testing logout issue navbar commant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
   return (
     <div>
       {/* <ClientComponent> */}
-      <Navbar searchBar={true} />
       <Hero />
       <Marquee />
       <PopularPlaces />
